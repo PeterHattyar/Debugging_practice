27cdf6cc8cc536c77562a59ac873234ab6d35107
Peter Hattyar <peter.hattyar@gmail.com>
2022-03-10 14:27:36 +0000

    Initial added broken code

--- burning_roof.rb
+++ burning_roof.rb
@@ -10,3 +10,13 @@ def get_most_common_letter(text)
   # 
   # > get_most_common_letter("the roof, the roof, the roof is on fire!")
   # => "o"
+
+  
+  # Original broken code
+  #   def get_most_common_letter(text)
+  #     counter = Hash.new(0)
+  #     text.chars.each do |char|
+  #       counter[char] += 1
+  #     end
+  #     counter.to_a.sort_by { |k, v| v }[0][0]
+  #   end
